57b4cb44e7bef6a1213dca80cea63652e3b367e6
PaRat07 <pasha.at.phone@gmail.com>
2023-08-24 14:43:56 +0300

    it-works

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
 import serial
+import time
 
 
 class Marker:
-    def __init__(self, port="COM1", timeout=0.1, badurate=115200):
+    def __write_to_marker(self, message):
+        self.ser.write(bytes(message + ';;', 'utf-8'))
+
+    def __read_from_marker(self) -> str:
+        ans = ""
+        postfix = self.ser.read(2).decode()
+        while postfix != ';;':
+            ans += postfix[0]
+            postfix = postfix[1] + self.ser.read().decode()
+
+        return ans
+
+    def __init__(self, port="COM5", timeout=0.1, badurate=115200):
         self.ser = serial.Serial(port=port, timeout=timeout, baudrate=badurate)
 
     def __del__(self):
         self.ser.close()
 
+    def set_pen_parameter(self, pen_id=0, mark_speed=1000, power=100):
+        self.__write_to_marker('SetPen,ID,' + str(pen_id) + ';MarkSpeed,' + str(mark_speed) + ';Power,' + str(power))
+        time.sleep(0.5)
+        print('Settings pen:', self.__read_from_marker())
+
+    def mark(self, file: str):
+        self.__write_to_marker('OpenDoc,' + file)
+        print('OpenDoc:', self.__read_from_marker())
+        self.set_pen_parameter()
+        self.__write_to_marker('StartMark')
+        print('Start mark:', self.__read_from_marker())
+        time.sleep(0.5)
+        self.__write_to_marker('StopMark')
+        print('StopMark:', self.__read_from_marker())
+
 
 if __name__ == '__main__':
-    pass
+    m = Marker()
+    m.mark('test.bpd')
